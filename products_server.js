f76dc6d9934ac6ec7b82b8af4f9bc8f97838502e
wesley <wesleynworisa@gmail.com>
2022-08-30 10:59:42 -0700

    added a few more server features

--- products_server.js
+++ products_server.js
@@ -1,7 +1,9 @@
-import { ladders } from "./products/data/ladders.db"
 import express from "express"
 import cors from "cors"
 import mysql from "mysql"
+// import {bodyparser} from "body-parser"
+const bodyParser = require('body-parser');
+
 
 var db = mysql.createConnection({
     host: "localhost",
@@ -9,7 +11,6 @@ var db = mysql.createConnection({
     user:'root',
     password:"",
     database:"ladders_db",
-    // TODO  make an sql database for this 
 })
 
 
@@ -23,6 +24,7 @@ console.log("server active")
 
 const server = express()
 server.use(cors())
+server.use(bodyParser.json());
 
 
 server.get("/",(req,res)=>{
@@ -30,18 +32,97 @@ server.get("/",(req,res)=>{
 })
 
 server.get("/products",(req,res) =>{
-    db.query("CALL `getAll`",function(err,result){
+    db.query("CALL `get_active`",function(err,result){
         if(err) throw err;
         res.json(result[0])
     })
     // res.json(ladders)
 })
+server.get("/admin/products",(req,res) =>{
+    db.query("CALL `getAll`",function(err,result){
+        if(err) throw err;
+        res.json(result[0])
+    })
+})
 
 server.get("/products/:id",(req,res)=>{
     let id = req.params.id
+    console.log(id)
     db.query("CALL `getProduct`("+id+")",function(err,result){
         if(err) throw err;
         res.json(result[0])
+    })
+})
+
+
+server.post("/admin/add",(req,res)=>{
+    let prod = req.body
+    console.log(req.body)
+    let name = prod.name
+    let pd = prod.price_d
+    let pc = prod.price_c
+    let avail = prod.availibility
+    let stock = prod.restock_Date
+    let img = prod.img
+    let desc = prod.description
+
+    let query= "CALL `add_product`(?,?,?,?,?,?,?)"
+    db.query(query,[name,pd,pc,avail,stock,img,desc],(err,result)=>{
+        if(err) res.send(err)
+        else{
+            res.send("created")
+        }
+    })
+})
+
+server.post("/admin/login",(req,res)=>{
+    console.log(req.body)
+    // let login = req.body
+    let name = req.body.name;
+    let pass = req.body.pass;
+    let query = "CALL `login`(?,?)"
+    db.query(query,[name,pass],function(err,result){
+        // console.log(result)
+        if(err) res.send(err);
+        res.json(result[0][0])
+    })
+})
+
+server.post("/admin/update",(req,res)=>{
+    let id = req.body.Id
+    let name = req.body.name
+    let pd = req.body.price_d
+    let pc = req.body.price_c
+    let avail = req.body.avalibility
+    let dat = req.body.restock_Date
+    let img = req.body.img_url
+    let des = req.body.description
+    let active = req.body.active
+    console.log(req.body)
+    let query = "CALL`update`(?,?,?,?,?,?,?,?)"
+    db.query(query,[id,name,pd,pc,avail,dat,img,des,active],(err,data)=>{
+        if(err)
+        {
+            res.send(err)
+        }
+        else
+        {
+            res.send("updated successfuly")
+        }
+    })
+
+
+})
+
+server.delete("/admin/delete/:id",(req,res)=>{
+    let id = req.params.id
+    let query = "CALL `del`(?)"
+    db.query(query,[id],(err,data)=>{
+        if(err) throw err;
+        else
+        {
+            res.send("deleted successfully")
+        }
     })
 })
 
